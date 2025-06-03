14b0a6acaee844d5272f6baf908222b6149d592d
htx2v <huynhvy.dev@gmail.com>
2025-06-03 20:26:21 +0700

    Use zod to validate requests

--- src/routes/contacts.router.js
+++ src/routes/contacts.router.js
@@ -1,20 +1,92 @@
 const express = require("express");
+const { z } = require("zod");
 
 const contactsController = require("../controllers/contacts.controller");
 const { methodNotAllowed } = require("../controllers/errors.controller");
+const { validateRequest } = require("../middlewares/validator.middleware");
+const {
+  contactSchema,
+  partialContactSchema,
+} = require("../schemas/contact.schemas");
 
 const router = express.Router();
 
 module.exports.setup = (app) => {
   app.use("/api/v1/contacts", router);
 
-  router.get("/", contactsController.getContactsByFilter);
-  router.post("/", contactsController.createContact);
+  router.get(
+    "/",
+    validateRequest(
+      z.object({
+        input: z
+          .object({
+            name: z.string().max(255).optional(),
+            favorite: z.enum(["true", "false"]).optional(),
+            page: z.coerce.number().nonnegative().default(1),
+            limit: z.coerce.number().nonnegative().default(5),
+          })
+          .strict(),
+      })
+    ),
+    contactsController.getContactsByFilter
+  );
+
+  router.post(
+    "/",
+    validateRequest(
+      z.object({
+        input: contactSchema.omit({ id: true, avtar: true }).strict(),
+      })
+    ),
+    contactsController.createContact
+  );
   router.delete("/", contactsController.deleteAllContacts);
   router.all("/", methodNotAllowed);
 
-  router.get("/:id", contactsController.getContact);
-  router.put("/:id", contactsController.updateContact);
-  router.delete("/:id", contactsController.deleteContact);
+  router.get(
+    "/:id",
+    validateRequest(
+      z.object({
+        input: contactSchema.pick({ id: true }).strict(),
+      })
+    ),
+    contactsController.getContact
+  );
+  router.put(
+    "/:id",
+    validateRequest(
+      z.object({
+        input: partialContactSchema
+          .omit({ avatar: true })
+          .strict()
+          .refine(
+            ({ name, email, address, phone, favorite, avatarFile }) => {
+              return (
+                name ||
+                email ||
+                address ||
+                phone ||
+                favorite !== undefined ||
+                avatarFile
+              );
+            },
+            {
+              message: "At least one field is required",
+            }
+          ),
+      })
+    ),
+    contactsController.updateContact
+  );
+
+  router.delete(
+    "/:id",
+    validateRequest(
+      z.object({
+        input: contactSchema.pick({ id: true }).strict(),
+      })
+    ),
+    contactsController.deleteContact
+  );
   router.all("/:id", methodNotAllowed);
 };
